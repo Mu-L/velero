39c745ef612eb81b77ebe64d854f8d27573b5e53
Lyndon-Li <lyonghui@vmware.com>
2026-06-02 17:25:41 +0800

    set totalSize from uploader
    Signed-off-by: Lyndon-Li <lyonghui@vmware.com>

--- pkg/uploader/block/uploader.go
+++ pkg/uploader/block/uploader.go
@@ -127,6 +127,7 @@ func (bu *blockUploader) Backup(source sourceInfo, parentObject udmrepo.ID, bitm
 		StartTime:   snapStart,
 		EndTime:     snapEnd,
 		Description: source.realSource,
+		TotalSize:   objectSize,
 		RootObject: udmrepo.ObjectMetadata{
 			ID:          entryId,
 			Name:        "bdev-root",
